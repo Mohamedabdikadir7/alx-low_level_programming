ec6b1588d7441e7d018d41461a516caf9b2cf197
Mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2023-10-27 01:59:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main-program that prints a line
  * return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
-}	
+}
