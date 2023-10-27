d26eed8c59ffbb2b8278165f96d18444230970fb
Mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2023-10-27 01:37:41 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(MyStr);
+
+	return (0);
+}
